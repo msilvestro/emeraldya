b7fb75ce3525d9680b450f38b146b8ea42a63e89
Matteo Silvestro <matteosilvestro@live.it>
2024-03-15 00:25:46 +0100

    Add Satori-inspired tooltips

--- converter.py
+++ converter.py
@@ -3,60 +3,74 @@ from builder import Sentence
 
 def convert(sentences: list[Sentence]):
     html_output = """<style>
-#sentences {
-    margin: 0 auto;
-    width: 60%;
-    font-size: 1.5em;
-}
+    #sentences {
+        margin: 0 auto;
+        max-width: 800px;
+        font-size: 1.5em;
+        position: relative;
+    }
 
-.word {
-    display: inline-block;
-}
+    .word {
+        display: inline-block;
+    }
 
-.tooltip {
-  position: relative;
-  border-bottom: 1px dotted black; 
-}
+    .tooltip {
+        position: relative;
+        border-bottom: 1px dotted black;
+    }
 
-.tooltip .tooltiptext {
-  visibility: hidden;
-  width: 120px;
-  background-color: black;
-  color: #fff;
-  text-align: center;
-  padding: 5px 0;
-  border-radius: 6px;
- 
-  position: absolute;
-  z-index: 1;
-  
-  width: 120px;
-  top: 100%;
-  left: 50%;
-  margin-left: -60px; /* Use half of the width (120/2 = 60), to center the tooltip */
-}
+    .tooltip:hover {
+        border-bottom: 1px solid black;
+    }
 
-.tooltip:hover .tooltiptext {
-  visibility: visible;
-}
+    #tooltip-container {
+        box-sizing: border-box;
+        position: absolute;
+        z-index: 1;
+        min-width: 100%;
+        padding: 10px;
+        border: 1px solid black;
+        background-color: white;
+    }
+
+    #tooltip-container .tooltip-close {
+        cursor: pointer;
+        background-color: black;
+        color: white;
+    }
 </style>
-<div id="sentences">"""
+<script>
+    function showTooltip(element, content) {
+        const tooltipContainer = document.getElementById("tooltip-container");
+        tooltipContainer.getElementsByClassName("tooltip-content")[0].innerText = content;
+        tooltipContainer.style.visibility = 'visible';
+        tooltipContainer.style.top = (window.scrollY + element.getBoundingClientRect().bottom - 8) + "px";
+    }
+
+    function hideTooltip() {
+        const tooltipContainer = document.getElementById("tooltip-container");
+        tooltipContainer.style.visibility = 'hidden';
+    }
+</script>
+<div id="sentences">
+    <div id="tooltip-container" style="visibility: hidden">
+        <span class="tooltip-close" onclick="hideTooltip()">[x]</span><br />
+        <span class="tooltip-content">This is the content of the tooltip</span>
+    </div>"""
     for sentence in sentences:
         if sentence is None:
             html_output += "<br />"
             continue
         for word in sentence.words:
-            classes = ["word"]
             if word.notes:
-                classes.append("tooltip")
-            classes_text = " ".join(classes)
-            html_output += f'<div class="{classes_text}">'
+                div_configuration = f' class="word tooltip" onclick="showTooltip(this, \'{word.notes}\')"'
+            else:
+                div_configuration = ' class="word"'
+            html_output += f"<div{div_configuration}>"
             html_output += f"<ruby>{word.writing}"
             if word.reading:
                 html_output += f"<rt>{word.reading}</rt>"
             html_output += "</ruby>"
-            if word.notes:
-                html_output += f'<span class="tooltiptext">{word.notes}</span>'
             html_output += "</div>"
         html_output += "<br />"
     html_output += "</div>"
