cf0c727eec420d8175f8674f617bdf22bc30a507
Matteo Silvestro <matteosilvestro@live.it>
2024-03-16 02:14:33 +0100

    Make tooltips more good-looking

--- converter.py
+++ converter.py
@@ -1,10 +1,12 @@
+from dataclasses import dataclass
+
 from furigana import split_characters
 from processor import Sentence
 
 
 def convert(header: dict, body: list[Sentence]):
     html_output = """<style>
-        #sentences {
+    #wrapper {
         margin: 0 auto;
         max-width: 800px;
         font-size: 1.5em;
@@ -15,11 +17,17 @@ def convert(header: dict, body: list[Sentence]):
         display: inline-block;
         border-bottom: 5px solid white;
         height: 1.5em;  /* otherwise kanjis might have different heights */
-        margin-bottom: 20px;
+        margin-top: 0.5em;
+        margin-bottom: 10px;
     }
     
     .word:hover {
-        border-bottom: 5px solid black;
+        border-bottom: 5px solid cornflowerblue;
+    }
+    
+    .translation {
+        display: inline-block;
+        cursor: pointer;
     }
 
     /* Customize ruby appearance to avoid spacing issues */
@@ -42,21 +50,47 @@ def convert(header: dict, body: list[Sentence]):
         position: absolute;
         z-index: 1;
         min-width: 100%;
-        padding: 10px;
         border: 1px solid black;
         background-color: white;
     }
 
+    #tooltip .section {
+        margin-bottom: 10px;
+    }
+
+    #tooltip .section:last-child {
+        margin-bottom: 0;
+    }
+
+    #tooltip .section-header {
+        background-color: cornflowerblue;
+        padding: 10px;
+        color: white;
+    }
+
+    #tooltip .section-content {
+        padding: 10px;
+    }
+    
+    #tooltip .section-content p {
+        margin: 0;
+        padding: 0;
+    }
+
     #tooltip .tooltip-close {
         cursor: pointer;
-        background-color: black;
-        color: white;
+        float: right;
+        line-height: 100%;
+    }
+
+    #tooltip ruby {
+        margin-top: 0.5em;
     }
 </style>
 <script>
     function showTooltip(element, content) {
         const tooltip = document.getElementById("tooltip");
-        tooltip.getElementsByClassName("tooltip-content")[0].innerHTML = content;
+        tooltip.innerHTML = content;
         tooltip.style.visibility = 'visible';
         tooltip.style.top = (window.scrollY + element.getBoundingClientRect().bottom - 8) + "px";
     }
@@ -65,23 +99,24 @@ def convert(header: dict, body: list[Sentence]):
         const tooltipContainer = document.getElementById("tooltip");
         tooltipContainer.style.visibility = 'hidden';
     }
-</script><div id="sentences">"""
+</script><div id="wrapper">"""
     html_output += "<h1>" + header["title"] + " - " + header["author"] + "</h1>"
     html_output += """
     <div id="tooltip" style="visibility: hidden">
-        <span class="tooltip-close" onclick="hideTooltip()">[x]</span><br />
-        <span class="tooltip-content">This is the content of the tooltip</span>
     </div>"""
     for sentence in body:
         if sentence is None:
             html_output += "<br />"
             continue
         for word in sentence.words:
-            html_output += f'<div class="word tooltip" onclick="showTooltip(this, \'{process_tooltips(word.tooltips)}\')">'
+            html_output += f'<div class="word" onclick="showTooltip(this, \'{process_tooltips(word.tooltips)}\')">'
             html_output += write_ruby(word.writing, word.reading)
             html_output += "</ruby>"
             html_output += "</div>"
-        html_output += f' <div class="word tooltip" onclick="showTooltip(this, \'{process_translation(sentence.translation)}\')">🔄</div>'
+        html_output += (
+            f' <div class="translation" title="translation"'
+            f" onclick=\"showTooltip(this, '{process_translation(sentence.translation)}')\">🔄</div>"
+        )
         html_output += "<br />"
     html_output += "</div>"
     return html_output
@@ -102,19 +137,38 @@ def write_ruby(writing, reading=None):
     return html_output
 
 
-def process_tooltips(tooltips):
-    output_html = ""
-    for tooltip in tooltips:
-        output_html += f"<b>{tooltip.title}</b><br />"
-        output_html += (
-            f"<span>{write_ruby(tooltip.writing, tooltip.reading)}</span><br />"
-        )
-        output_html += f"<span>{tooltip.content}</span><br />"
+@dataclass
+class TooltipSection:
+    title: str
+    content: str
+
+
+def create_tooltip_sections(*sections: TooltipSection):
+    output_html = "<div class='section'>"
+    is_first_header = True
+    for section in sections:
+        output_html += f"<div class='section-header'><span>{section.title}</span>"
+        if is_first_header:
+            output_html += (
+                "<span class='tooltip-close' onclick='hideTooltip()'>x</span>"
+            )
+            is_first_header = False
+        output_html += "</div>"
+        output_html += f"<div class='section-content'>{section.content}</div>"
+    output_html += "</div>"
     return output_html.replace("'", "\\'")
+
+
+def process_tooltips(tooltips):
+    sections = []
+    for tooltip in tooltips:
+        content = f"<p>{write_ruby(tooltip.writing, tooltip.reading)}</p>"
+        content += f"<p>{tooltip.content}</p>"
+        sections.append(TooltipSection(title=tooltip.title, content=content))
+    return create_tooltip_sections(*sections)
 
 
 def process_translation(translation):
-    output_html = ""
-    output_html += "<b>Translation</b><br />"
-    output_html += f"<span>{translation}</span>"
-    return output_html.replace("'", "\\'")
+    return create_tooltip_sections(
+        TooltipSection(title="Translation", content=translation)
+    )
