2f5c7e2a6cd9d0738f9047050639df9e7932c2be
Matteo Silvestro <matteosilvestro@live.it>
2024-03-20 01:47:27 +0100

    Make command more flexible

--- emeraldya/main.py
+++ emeraldya/main.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+
 from emeraldya.processor import process
 from emeraldya.converter import convert
 
 
 def run():
-    with open("haru.em", "r", encoding="utf-8") as reader:
-        em_input = reader.read()
+    for em_file in Path().glob("*.em"):
+        with open(em_file, "r", encoding="utf-8") as reader:
+            em_input = reader.read()
 
-    header, body = process(em_input)
-    html_output = convert(header, body)
+        header, body = process(em_input)
+        html_output = convert(header, body)
 
-    with open("index.html", "w", encoding="utf-8") as writer:
-        writer.write(html_output)
+        html_file = em_file.with_suffix(".html")
+        with open(html_file, "w", encoding="utf-8") as writer:
+            writer.write(html_output)
