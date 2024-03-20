36049631b5825b3e911243b5d9a4665f7b00f13f
Matteo Silvestro <matteosilvestro@live.it>
2024-03-20 01:59:55 +0100

    Make command customizable

--- emeraldya/main.py
+++ emeraldya/main.py
@@ -1,17 +1,28 @@
 from pathlib import Path
 
+import click
+
 from emeraldya.processor import process
 from emeraldya.converter import convert
 
 
-def run():
-    for em_file in Path().glob("*.em"):
+@click.command()
+@click.option("--input-dir", "-i", default=".")
+@click.option("--output-dir", "-o", default=".")
+def run(input_dir: str, output_dir: str):
+    file_found = False
+    for em_file in Path(input_dir).glob("*.em"):
+        file_found = True
+        click.echo(f"Found file '{em_file}'...")
         with open(em_file, "r", encoding="utf-8") as reader:
             em_input = reader.read()
 
         header, body = process(em_input)
         html_output = convert(header, body)
 
-        html_file = em_file.with_suffix(".html")
+        html_file = Path(output_dir) / em_file.with_suffix(".html").name
         with open(html_file, "w", encoding="utf-8") as writer:
             writer.write(html_output)
+        click.echo(f"> Converted to '{html_file}'")
+    if not file_found:
+        click.echo("No emerald files found")
